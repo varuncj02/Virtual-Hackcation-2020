0b95880a46e0b9fb33ac6744942c6ae5778d79fa
karthikv77 <67761087+karthikv77@users.noreply.github.com>
2020-07-03 22:30:56 -0500

    Made a frontPage dart file

---
+++ lib/frontPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class FrontPage extends StatefulWidget {
+  @override
+  _FrontPageState createState() => _FrontPageState();
+}
+
+class _FrontPageState extends State<FrontPage> {
+  //Widget build;
+
+}
