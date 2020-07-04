ae646a140fb47a8fcf201e431bf6c3e119615991
varuncj02 <varun.joshi.2016.go@gmail.com>
2020-07-04 01:50:47 -0700

    Created the main page ui completely end to end with sample data

--- lib/frontPage.dart
+++ lib/frontPage.dart
@@ -4,23 +4,66 @@ import 'dart:math';
 
 //import IconButton from '@material-ui/core/IconButton';
 
+/**
+ * So the Base UI or layout is done 
+ * Tasks to Do with this Class File or Around it:
+ * 1. In the Camera Button we write navigation code to the Flutter Camera
+ * 2. We need to Find a way to instead of hardcoded lists import a prepared list
+ * 3. In a continuation of point 2 basically find a way to store the image captured - way to do that is to use 
+ *    the google libraries to create an automatic file path and then store that file path as a List of Strings 
+ *    in some other classes
+ */
+
 class FrontPage extends StatefulWidget {
   @override
   _FrontPageState createState() => _FrontPageState();
 }
 
 class _FrontPageState extends State<FrontPage> {
-  //Widget build;
+  // This list will just copy the 2 lists from the Camera stored by Bharath
+  // The copying happens by enterting the 2 lists as parameters in constructors
+  // Right now these are just hardcoded values
+  List<String> sampleTops = [
+    "images/gucci.jpg",
+    "images/shirt1.jpg",
+    "images/shirt2.jpg",
+  ];
 
-  void _incrementListItem() {
+  //Same Logic Needed Above
+  List<String> sampleBottoms = [
+    "images/jeans.jpg",
+    "images/jeans2.jpg",
+    "images/jeans3.jpg",
+  ];
+
+  int topIndex = 0;
+  int bottomIndex = 0;
+
+  void _incrementListItemTop() {
     setState(() {
-      // This is where the List index is gonna be incremented
+      // This is where the List index for top is gonna be incremented
+      topIndex++;
     });
   }
 
-  void _decrementListItem() {
+  void _decrementListItemTop() {
     setState(() {
-      // This is where the list Index is gonna be decremented
+      // This is where the list Index for top is gonna be decremented
+      topIndex--;
+    });
+  }
+
+  void _incrementListItemBottom() {
+    setState(() {
+      // This is where the List index for Bottom is gonna be incremented
+      bottomIndex++;
+    });
+  }
+
+  void _decrementListItemBottom() {
+    setState(() {
+      // This is where the list Index for Bottom is gonna be decremented
+      bottomIndex--;
     });
   }
 
@@ -28,38 +71,105 @@ class _FrontPageState extends State<FrontPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('App Screen'),
       ),
       body: Align(
         alignment: Alignment.topCenter,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: Transform.rotate(
-                angle: 180 * pi / 180,
-                child: IconButton(
-                    icon: Icon(Icons.play_arrow),
-                    onPressed: () {
-                      _incrementListItem();
-                    }),
+        child: Container(
+          margin: EdgeInsets.fromLTRB(10, 80, 0, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Transform.rotate(
+                      angle: 180 * pi / 180,
+                      child: IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            _decrementListItemTop();
+                          }),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    //child: Image.asset("images/gucci.jpg"),
+                    child: Image.asset(sampleTops[topIndex]),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                        icon: Icon(Icons.play_arrow),
+                        onPressed: () {
+                          _incrementListItemTop();
+                          // This is where when u click u go one clothing item behind so in terms of structure in the list
+                          // it means one behind. This List is being developed by Bharath
+                        }),
+                  ),
+                ],
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Image.asset("images/gucci.jpg"),
-            ),
-            Expanded(
-              child: IconButton(
-                  icon: Icon(Icons.play_arrow),
-                  onPressed: () {
-                    _decrementListItem();
-                    // This is where when u click u go one clothing item behind so in terms of structure in the list
-                    // it means one behind. This List is being developed by Bharath
-                  }),
-            ),
-          ],
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Transform.rotate(
+                      angle: 180 * pi / 180,
+                      child: IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            _decrementListItemBottom();
+                          }),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset(sampleBottoms[bottomIndex]),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                        icon: Icon(Icons.play_arrow),
+                        onPressed: () {
+                          _incrementListItemBottom();
+                          // This is where when u click u go one clothing item behind so in terms of structure in the list
+                          // it means one behind. This List is being developed by Bharath
+                        }),
+                  ),
+                ],
+              ),
+              Expanded(
+                  child: Container(
+                margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+                child: Row(
+                  //mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Expanded(
+                      child: FlatButton(
+                        color: Colors.blueAccent,
+                        onPressed: () {
+                          // Code to Navigate to the Camera Plugin from Karthik Mohans Code
+                          //Gonna use Navigate Push
+                        },
+                        // Navigate to a specific Top Camera
+                        child: Text('Top Camera'),
+                      ),
+                    ),
+                    Expanded(
+                      child: FlatButton(
+                        color: Colors.blueAccent,
+                        onPressed: () {
+                          // Code to Navigate to the Camera Plugin from Karthik Mohans Code
+                          //Gonna use Navigate Push
+                        },
+                        // Navigate to a specific Bottom Camera
+                        child: Text('Bottom Camera'),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+            ],
+          ),
         ),
       ),
     );
